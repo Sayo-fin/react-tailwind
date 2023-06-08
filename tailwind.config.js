d13b07dac469201175f09420b2e051a41f7c3ce2
Sayo <104796045+Sayo-fin@users.noreply.github.com>
2023-06-08 22:50:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const { default: plugin } = require("tailwindcss");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    sm: "640px",
+
+    md: "768px",
+
+    lg: "1024px",
+
+    xl: "1280px",
+
+    "2xl": "1536px",
+  },
+  plugins: [],
+};
